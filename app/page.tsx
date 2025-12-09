1b6945f7e3b132069929f481c48d2a502e27b3db
Rikhil Arora <rikhilcooldude@gmail.com>
2025-12-09 04:00:57 +0100

    refactor: move code to seminars folder

--- app/page.tsx
+++ app/page.tsx
@@ -19,8 +19,8 @@ export default function AppHeader() {
         Session →
       </Link>
 
-      <Link href="/speakers" className="btn btn-outline-primary">
-        Speakers →
+      <Link href="/seminars" className="btn btn-outline-primary">
+        Seminars →
       </Link>
     </AppContainer>
   );
